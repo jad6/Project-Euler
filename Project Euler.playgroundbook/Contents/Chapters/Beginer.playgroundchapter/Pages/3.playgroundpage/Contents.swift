b5243a5eb958ca435b955b7835a8104424a78a88
Jad Osseiran <jad6@users.noreply.github.com>
2016-10-08 13:43:12 -0700

    Added screenshot images

--- Project Euler.playgroundbook/Contents/Chapters/Beginer.playgroundchapter/Pages/3.playgroundpage/Contents.swift
+++
@@ -1,8 +0,0 @@
-/*:
- # Largest prime factor
- [Problem 3](https://projecteuler.net/problem=3) - Diifculty level: 5%
-
- The prime factors of 13195 are 5, 7, 13 and 29.
-
- What is the largest prime factor of the number 600851475143?
- */
--- Project Euler.playgroundbook/Contents/Chapters/Beginer.playgroundchapter/Pages/5.playgroundpage/Contents.swift
+++
@@ -1,8 +0,0 @@
-/*:
- # Smallest multiple
- [Problem 5](https://projecteuler.net/problem=5) - Diifculty level: 5%
-
- 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-
- What is the smallest positive number that is [evenly divisible](glossary://evenly%20divisible) by all of the numbers from 1 to 20?
- */
