ee128bad44ae08a068e2e37e7455c3e34aa25382
Jad Osseiran <jad6@icloud.com>
2016-10-08 12:38:54 -0700

    cp

---
+++ Project Euler.playgroundbook/Contents/Chapters/Beginer.playgroundchapter/Pages/6.playgroundpage/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+ # Sum square difference
+ [Problem 6](https://projecteuler.net/problem=6) - Diifculty level: 5%
+
+ The sum of the squares of the first ten natural numbers is,
+
+ ![](6-1.png)
+
+ The square of the sum of the first ten natural numbers is,
+
+ ![](6-2.png)
+
+ Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+ Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
--- Project Euler.playgroundbook/Contents/Chapters/Hard.playgroundchapter/Pages/259.playgroundpage/Contents.swift
+++ Project Euler.playgroundbook/Contents/Chapters/Hard.playgroundchapter/Pages/259.playgroundpage/Contents.swift
@@ -8,7 +8,7 @@
  - Any successive digits can be concatenated (for example, using the digits 2, 3 and 4 we obtain the number 234).
  - Only the four usual binary arithmetic operations (addition, subtraction, multiplication and division) are allowed.
  - Each operation can be used any number of times, or not at all.
- - Unary minus is not allowed.
+ - [Unary minus](glossary://unary%20minus) is not allowed.
  - Any number of (possibly nested) parentheses may be used to define the order of operations.
 
  For example, 42 is reachable, since (1/23) * ((4*5)-6) * (78-9) = 42.
